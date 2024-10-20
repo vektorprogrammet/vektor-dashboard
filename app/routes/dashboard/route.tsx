3c009d2290f8511f575650d3c7debd0cd3f2fc13
phibkro <71797726+phibkro@users.noreply.github.com>
2024-10-20 02:57:15 +0200

    chore: :sparkles: demo sidebar-07

--- app/routes/dashboard/route.tsx
+++ app/routes/dashboard/route.tsx
@@ -1,34 +1,36 @@
 import { Link, Outlet, redirect } from "@remix-run/react";
-import { ActionToggle } from "./ActionToggle";
 import { AppShell, Burger, Group } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { MantineLogo } from "@mantinex/mantine-logo";
 import { useState } from "react";
 import { UnstyledButton, Tooltip, Title, rem } from "@mantine/core";
-import {
-  IconHome2,
-  IconGauge,
-  IconDeviceDesktopAnalytics,
-  IconFingerprint,
-  IconCalendarStats,
-  IconUser,
-  IconSettings,
-  IconUsers,
-} from "@tabler/icons-react";
-import classes from "./DoubleNavbar.module.css";
 import { useClickOutside } from "@mantine/hooks";
 import { useAuth } from "@clerk/remix";
-
-const mainLinksMockdata = [
-  { icon: IconHome2, label: "Home", route: "/" },
-  { icon: IconGauge, label: "Dashboard" },
-  { icon: IconUsers, label: "Users", route: "/users" },
-  { icon: IconDeviceDesktopAnalytics, label: "Analytics" },
-  { icon: IconCalendarStats, label: "Releases" },
-  { icon: IconUser, label: "Account" },
-  { icon: IconFingerprint, label: "Security" },
-  { icon: IconSettings, label: "Settings" },
-];
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarInset,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  BreadcrumbPage,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@radix-ui/react-separator";
 
 const routes = [
   { name: "Home", path: "/home" },
@@ -41,92 +43,8 @@ const routes = [
   { name: "Interesse", path: "/interest" },
 ];
 
-const linksMockdata = [
-  "Security",
-  "Settings",
-  "Dashboard",
-  "Releases",
-  "Account",
-  "Orders",
-  "Clients",
-  "Databases",
-  "Pull Requests",
-  "Open Issues",
-  "Wiki pages",
-];
-
-function DoubleNavbar() {
-  const [active, setActive] = useState("Releases");
-  const [activeLink, setActiveLink] = useState("Settings");
-  const [opened, setOpened] = useState(false);
-  const ref = useClickOutside(() => setOpened(false));
-
-  const mainLinks = mainLinksMockdata.map((link) => {
-    const button = (
-      <Tooltip
-        label={link.label}
-        position="right"
-        withArrow
-        transitionProps={{ duration: 0 }}
-        key={link.label}
-      >
-        <UnstyledButton
-          onClick={() => setActive(link.label)}
-          className={classes.mainLink}
-          data-active={link.label === active || undefined}
-        >
-          <link.icon style={{ width: rem(22), height: rem(22) }} stroke={1.5} />
-        </UnstyledButton>
-      </Tooltip>
-    );
-    if (link.route) {
-      return (
-        <Link to={link.route} key={link.label}>
-          {button}
-        </Link>
-      );
-    }
-    return button;
-  });
-
-  const links = linksMockdata.map((link) => (
-    <a
-      className={classes.link}
-      data-active={activeLink === link || undefined}
-      href="#"
-      onClick={(event) => {
-        event.preventDefault();
-        setActiveLink(link);
-      }}
-      key={link}
-    >
-      {link}
-    </a>
-  ));
-
-  return (
-    <nav className={classes.navbar}>
-      <div className={classes.wrapper}>
-        <div className={classes.aside}>
-          <div className={classes.logo}>
-            <MantineLogo type="mark" size={30} />
-          </div>
-          {mainLinks}
-        </div>
-        <div className={classes.main}>
-          <Title order={4} className={classes.title}>
-            {active}
-          </Title>
-
-          {links}
-        </div>
-      </div>
-    </nav>
-  );
-}
-
 export default function Layout() {
-  const [opened, { toggle }] = useDisclosure();
+  /* const [opened, { toggle }] = useDisclosure();
   const { isLoaded, userId } = useAuth();
 
   if (!isLoaded) {
@@ -135,9 +53,44 @@ export default function Layout() {
 
   if (!userId) {
     return redirect("/sign-in");
-  }
-
+  } */
   return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <Outlet />
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+          </div>
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+
+  /* return (
     <AppShell
       padding="md"
       header={{
@@ -166,5 +119,5 @@ export default function Layout() {
         <Outlet />
       </AppShell.Main>
     </AppShell>
-  );
+  ); */
 }
