4bd63667e8dbf0c8404d484f4e08c80bebc9860c
phibkro <71797726+phibkro@users.noreply.github.com>
2024-10-20 21:28:09 +0200

    refactor: :white_check_mark: group related tests

--- app/pages/home.spec.ts
+++ app/pages/home.spec.ts
@@ -1,18 +1,22 @@
 import { expect, test } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  await page.goto("/dashboard");
+test.describe("Dashboard home page", () => {
+  test("has title", async ({ page }) => {
+    await page.goto("/dashboard");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Dashboard/);
-});
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Dashboard/);
+  });
 
-test("has heading", async ({ page }) => {
-  await page.goto("/dashboard");
+  test("has heading", async ({ page }) => {
+    await page.goto("/dashboard");
 
-  // Click the get started link.
-  // await page.getByRole("link", { name: "Get started" }).click();
+    // Click the get started link.
+    // await page.getByRole("link", { name: "Get started" }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole("heading", { name: "Dashboard" })).toBeVisible();
+    // Expects page to have a heading with the name of Installation.
+    await expect(
+      page.getByRole("heading", { name: "Dashboard" }),
+    ).toBeVisible();
+  });
 });
--- app/pages/layout.spec.ts
+++ app/pages/layout.spec.ts
@@ -6,18 +6,22 @@ const departments = {
   aas: "Ås",
 };
 
-test("mulig å velge alle avdelinger", async ({ page }) => {
-  await page.goto("http://localhost:5173/dashboard");
+test.describe("Dashboard sidebar", () => {
+  test.describe("department selection", () => {
+    test("all departments should be selectable", async ({ page }) => {
+      await page.goto("http://localhost:5173/dashboard");
 
-  await page.getByRole("button", { name: departments.trondheim }).click();
-  await page.getByRole("menuitem", { name: departments.bergen }).click();
-  await expect(page.locator("body")).toContainText(departments.bergen);
+      await page.getByRole("button", { name: departments.trondheim }).click();
+      await page.getByRole("menuitem", { name: departments.bergen }).click();
+      await expect(page.locator("body")).toContainText(departments.bergen);
 
-  await page.getByRole("button", { name: departments.bergen }).click();
-  await page.getByRole("menuitem", { name: departments.aas }).click();
-  await expect(page.locator("body")).toContainText(departments.aas);
+      await page.getByRole("button", { name: departments.bergen }).click();
+      await page.getByRole("menuitem", { name: departments.aas }).click();
+      await expect(page.locator("body")).toContainText(departments.aas);
 
-  await page.getByRole("button", { name: departments.aas }).click();
-  await page.getByRole("menuitem", { name: departments.trondheim }).click();
-  await expect(page.locator("body")).toContainText(departments.trondheim);
+      await page.getByRole("button", { name: departments.aas }).click();
+      await page.getByRole("menuitem", { name: departments.trondheim }).click();
+      await expect(page.locator("body")).toContainText(departments.trondheim);
+    });
+  });
 });
