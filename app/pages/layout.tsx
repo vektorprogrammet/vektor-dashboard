684528fd8ca51ce594cbc26031b29e170ee2d4ae
phibkro <71797726+phibkro@users.noreply.github.com>
2024-10-21 22:53:53 +0200

    refactor: :fire: remove duplicate avatar item

--- app/pages/layout.tsx
+++ app/pages/layout.tsx
@@ -90,19 +90,6 @@ function UserMenu({
             align="end"
             sideOffset={4}
           >
-            <DropdownMenuLabel className="p-0 font-normal">
-              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-                </Avatar>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
-                </div>
-              </div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
             <DropdownMenuGroup>
               <DropdownMenuItem>
                 <Icon icon="lucide:badge-check" />
