b87181009febdb224ff41c8fa45305d44dd4a925
phibkro <71797726+phibkro@users.noreply.github.com>
2024-10-22 01:01:54 +0200

    fix: :bug: fix StatusSelector styling

--- app/pages/layout.tsx
+++ app/pages/layout.tsx
@@ -448,9 +448,12 @@ function StatusSelector({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Icon icon="lucide:map-pinned" className="size-4 shrink-0" />
+              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-accent text-sidebar-accent-foreground">
+                <Icon icon="lucide:map-pinned" className="size-4 shrink-0" />
+              </div>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">{activeStatus}</span>
+                <span className="truncate text-xs">Avdeling</span>
               </div>
               <Icon icon="lucide:chevrons-up-down" className="ml-auto" />
             </SidebarMenuButton>
