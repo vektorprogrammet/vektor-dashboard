22031c508a1967177fd31e2059b470f858a2986e
phibkro <71797726+phibkro@users.noreply.github.com>
2024-10-22 11:22:04 +0200

    test: :white_check_mark: check that tooltips work on focus

--- app/pages/__tests__/layout.spec.ts
+++ app/pages/__tests__/layout.spec.ts
@@ -51,4 +51,39 @@ test.describe("Dashboard sidebar", () => {
       await expect(page.locator("body")).toContainText(departments.trondheim);
     });
   });
+
+  test.describe("tooltips funker", () => {
+    test.beforeEach(async ({ page }) => {
+      await page.getByRole("button", { name: "Toggle Sidebar" }).click();
+    });
+    test("for user menu", async ({ page }) => {
+      await page
+        .getByRole("button", { name: "CN shadcn m@example.com" })
+        .focus();
+      await page.getByText("User menuUser menu").focus();
+      await expect(page.getByText("User menuUser menu")).toBeVisible();
+    });
+    test("for primary navigation", async ({ page }) => {
+      await page.getByRole("button", { name: "Opptak" }).focus();
+      await expect(page.getByText("OpptakOpptak")).toBeVisible();
+      await page.getByRole("button", { name: "Statistikk" }).focus();
+      await expect(page.getByText("StatistikkStatistikk")).toBeVisible();
+      await page.getByRole("button", { name: "Informasjon" }).focus();
+      await expect(page.getByText("InformasjonInformasjon")).toBeVisible();
+      await page.getByRole("button", { name: "Data" }).focus();
+      await expect(page.getByText("DataData")).toBeVisible();
+      await page.getByRole("button", { name: "Annet" }).focus();
+      await expect(page.getByText("AnnetAnnet")).toBeVisible();
+    });
+    test("for secondary navigation", async ({ page }) => {
+      await page.getByRole("link", { name: "Support" }).focus();
+      await expect(page.getByText("SupportSupport")).toBeVisible();
+      await page.getByRole("link", { name: "Feedback" }).focus();
+      await expect(page.getByText("FeedbackFeedback")).toBeVisible();
+    });
+    test("for sidebar footer", async ({ page }) => {
+      await page.getByRole("button", { name: "Avdeling" }).focus();
+      await expect(page.getByText("AvdelingerAvdelinger")).toBeVisible();
+    });
+  });
 });
