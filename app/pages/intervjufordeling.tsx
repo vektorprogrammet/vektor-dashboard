223b4d4a59f7dfcae50542b1867aa486feac1554
Filip Skaug <filis@stud.ntnu.no>
2025-02-02 15:16:54 +0100

    vf-209 add table and data

--- app/pages/intervjufordeling.tsx
+++ app/pages/intervjufordeling.tsx
@@ -1,39 +1,21 @@
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
-
+import CreateTable from "@/components/create-table";
+import { data } from './data/intervjufordeling';
 
 const intervjufordeling = () => {
   return (
     <main className="w-screen md:w-full">
-      <Table className="overflow-clip">
-        <TableHeader>
-          <TableRow>
-            <TableHead className="">Søker</TableHead>
-            <TableHead>Telefon</TableHead>
-            <TableHead>E-post</TableHead>
-            <TableHead className="">Linje</TableHead>
-            <TableHead className="">År</TableHead>
-            <TableHead className="">Tildelt</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell className="">Nora Normann</TableCell>
-            <TableCell className="">1234567</TableCell>
-            <TableCell className="">noranormann@gmail.com</TableCell>
-            <TableCell className="">MTING</TableCell>
-            <TableCell className="">2 Klasse</TableCell>
-            <TableCell className="">Sven Svenske</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
+        <CreateTable
+          header={[
+            "Navn",
+            "Skole",
+            "E-post",
+            "Semester",
+            "Avdeling",
+            "Bolk",
+            "Dag",
+          ]}
+          content={data.Assistenter}
+        />
     </main>
   );
 }
--- app/root.tsx
+++ app/root.tsx
@@ -24,6 +24,8 @@ export function Layout({ children }: { children: ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/public/vektor-logo-circle.svg" />
+
         <Meta />
         <Links />
       </head>
