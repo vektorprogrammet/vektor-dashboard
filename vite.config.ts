99eec416b80a84b6006f2b0e03056e60a6c97ba5
phibkro <71797726+phibkro@users.noreply.github.com>
2025-02-06 11:01:57 +0100

    chore: :wrench: update shadcn/ui cli config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { reactRouter } from "@react-router/dev/vite";
+import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -7,4 +8,9 @@ export default defineConfig({
     reactRouter(),
     tsconfigPaths(),
   ],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./app"),
+		}
+	}
 });
