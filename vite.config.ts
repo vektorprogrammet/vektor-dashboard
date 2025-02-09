46e67319b58c58a877fbaa412a2aa64b58dc894d
phibkro <71797726+phibkro@users.noreply.github.com>
2025-02-10 00:34:42 +0100

    chore: :heavy_minus_sign: remove vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
 import path from "path";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     reactRouter(),
-    tsconfigPaths(),
   ],
 	resolve: {
 		alias: {
