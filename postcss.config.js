b93bc5159dff40a976cbe5aa6efc90bb276706fe
phibkro <71797726+phibkro@users.noreply.github.com>
2025-02-10 00:46:58 +0100

    refactor: :arrow_up: pnpm dlx @tailwindcss/upgrade@next

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    '@tailwindcss/postcss': {},
     "postcss-preset-mantine": {},
     "postcss-simple-vars": {
       variables: {
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from '@tailwindcss/vite';
 import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
     reactRouter(),
+		tailwindcss(),
   ],
 	resolve: {
 		alias: {
