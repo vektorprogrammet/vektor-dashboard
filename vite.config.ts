fdb0e350191495696fdf50895aff4e5d7e23ac10
phibkro <71797726+phibkro@users.noreply.github.com>
2025-03-03 23:07:04 +0100

    fix: :bug: fix shadcn ui cli import aliases
    Don't use path.__dirname for vite alias resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from '@tailwindcss/vite';
-import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -10,10 +9,10 @@ export default defineConfig({
   ],
 	resolve: {
 		alias: {
-			"@/components": path.resolve(__dirname, "./app/components"),
-			"@/hooks": path.resolve(__dirname, "./app/hooks"),
-			"@/lib": path.resolve(__dirname, "./app/lib"),
-			"@/ui": path.resolve(__dirname, "./app/components/ui"),
+			"@/components": "/app/components",
+			"@/hooks": "/app/hooks",
+			"@/lib": "/app/lib",
+			"@/ui": "/app/components/ui",
 		}
 	}
 });
