b8ac4ebe9b351b23188d65dd132cd916e3bf4471
Filip Skaug <46749200+FilipSkaug@users.noreply.github.com>
2025-03-12 16:44:59 +0100

    vf-256 fix lint errors (#3)
    * vf-256 fix lint errors
    
    * vf-256 fix linting
    
    * style: :rotating_light: capitalize types
    
    * style: :art: sort imports
    
    * style: :art: sort css classes
    
    ---------
    
    Co-authored-by: phibkro <71797726+phibkro@users.noreply.github.com>

--- app/components/data-table-pagination.tsx
+++ app/components/data-table-pagination.tsx
@@ -1,11 +1,3 @@
-import type { Table } from "@tanstack/react-table";
-import {
-  ChevronLeft,
-  ChevronRight,
-  ChevronsLeft,
-  ChevronsRight,
-} from "lucide-react";
-
 import { Button } from "@/components/ui/button";
 import {
   Select,
@@ -14,23 +6,30 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import type { Table } from "@tanstack/react-table";
+import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+} from "lucide-react";
 
-interface DataTablePaginationProps<TData> {
-  table: Table<TData>;
+interface DataTablePaginationProps<Data> {
+  table: Table<Data>;
 }
 
-export function DataTablePagination<TData>({
+export function DataTablePagination<Data>({
   table,
-}: DataTablePaginationProps<TData>) {
+}: DataTablePaginationProps<Data>) {
   return (
     <div className="flex items-center justify-between px-2">
-      <div className="flex-1 text-sm text-muted-foreground">
+      <div className="flex-1 text-muted-foreground text-sm">
         {table.getFilteredSelectedRowModel().rows.length} of{" "}
         {table.getFilteredRowModel().rows.length} row(s) selected.
       </div>
       <div className="flex items-center space-x-6 lg:space-x-8">
         <div className="flex items-center space-x-2">
-          <p className="text-sm font-medium">Rows per page</p>
+          <p className="font-medium text-sm">Rows per page</p>
           <Select
             value={`${table.getState().pagination.pageSize}`}
             onValueChange={(value) => {
@@ -49,7 +48,7 @@ export function DataTablePagination<TData>({
             </SelectContent>
           </Select>
         </div>
-        <div className="flex w-[100px] items-center justify-center text-sm font-medium">
+        <div className="flex w-[100px] items-center justify-center font-medium text-sm">
           Page {table.getState().pagination.pageIndex + 1} of{" "}
           {table.getPageCount()}
         </div>
